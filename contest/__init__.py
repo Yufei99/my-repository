55d540d868245c67efe4d282cb1b76431628431b
Yufei <laraine757@gmail.com>
2025-06-24 17:15:25 +0100

    compute outcome

--- contest/__init__.py
+++ contest/__init__.py
@@ -9,36 +9,90 @@ Your app description
 class C(BaseConstants):
     NAME_IN_URL = 'contest'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 1
+    NUM_ROUNDS = 2
+    ENDOWMENT = Currency(10)
+    COST_PER_TICKET = Currency(0.50)
+    PRIZE = Currency(8)
 
 
 class Subsession(BaseSubsession):
-    pass
+    is_paid = models.BooleanField()
 
+    def setup_round(self):
+        self.is_paid = True
+        for group in self.get_groups():
+            group.setup_round()
+    def compute_outcome(self):
+        for group in self.get_groups():
+            group.compute_outcome()
 
 class Group(BaseGroup):
-    pass
+    prize = models.CurrencyField()
+
+    def setup_round(self):
+        self.prize = C.PRIZE
+        for player in self.get_players():
+            player.setup_round()
+
+    def compute_outcome(self):
+        total = sum(player.tickets_purchased for player in self.get_players())
+        for player in self.get_players():
+            try:
+                player.prize_won = player.tickets_purchased / total
+            except ZeroDivisionError:
+                player.prize_won = 1 / len(self.get_players())
+            player.earnings =(
+                player.endowment -
+                player.tickets_purchased * player.cost_per_ticket +
+                self.prize * player.prize_won
+            )
 
 
 class Player(BasePlayer):
-    pass
+    endowment = models.CurrencyField()
+    cost_per_ticket = models.CurrencyField()
+    tickets_purchased = models.IntegerField()
+    prize_won = models.FloatField()
+    earnings = models.CurrencyField()
+
+
+    def setup_round(self):
+        self.endowment = C.ENDOWMENT
+        self.cost_per_ticket = C.COST_PER_TICKET
+
+    @property
+    def coplayer(self):
+        return self.group.get_player_by_id(3 - self.id_in_group)
 
 
 # PAGES
 class SetupRound(WaitPage):
-  pass
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        subsession.setup_round()
 
 class Intro(Page):
     pass
 
 class Decision(Page):
-    pass
+    form_model = 'player'
+    form_fields = ['tickets_purchased']
 
 class DecisionWaitPage(WaitPage):
-  pass
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession):
+        subsession.compute_outcome()
 
 class Results(Page):
-    pass
+    @staticmethod
+    def vars_for_template(player):
+        return {
+            'coplayer_purchased': player.group.get_player_by_id(3 - player.id_in_group).tickets_purchased,
+        }
 
 class EndBlock(Page):
     pass
